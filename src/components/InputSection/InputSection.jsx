7d2bad9cc50511d3b672aaac1a346eeaa06da85b
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-10-22 20:05:34 +0300

    add: Added ability to remove accent

--- src/components/InputSection/InputSection.jsx
+++ src/components/InputSection/InputSection.jsx
@@ -11,19 +11,27 @@ function InputSection() {
     }
 
     const handleClick = (word, letterIndex) => {
-        const accentedWord = word
-            .split('')
-            .map((letter, index) => {
-                if (index === letterIndex && isVowel(letter)) {
-                    return letter + '\u0301'
-                }
-                return letter
-            })
-            .join('')
+        if (isVowel(word[letterIndex]) && word[letterIndex + 1] !== '\u0301') {
+            const accentedWord = word
+                .split('')
+                .map((letter, index) => {
+                    if (index === letterIndex) {
+                        return letter + '\u0301'
+                    }
+                    return letter
+                })
+                .join('')
 
-        const newText = text.replace(word, accentedWord)
-        setText(newText)
-        setAccentuatedText(newText)
+            const newText = text.replace(word, accentedWord)
+            setText(newText)
+            setAccentuatedText(newText)
+        } else if (word[letterIndex + 1] === '\u0301') {
+            const deaccentedWord =
+                word.slice(0, letterIndex) + word[letterIndex] + word.slice(letterIndex + 2)
+            const newText = text.replace(word, deaccentedWord)
+            setText(newText)
+            setAccentuatedText(newText)
+        }
     }
 
     const renderStanza = (stanza) => {
