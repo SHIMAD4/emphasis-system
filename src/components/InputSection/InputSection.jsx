3c49a541bb44b7ca51c61d1676bdbf5220f9c3c0
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-10-22 22:19:40 +0300

    fix: Fixed the problem of accents in first occurrences

--- src/components/InputSection/InputSection.jsx
+++ src/components/InputSection/InputSection.jsx
@@ -9,31 +9,37 @@ function InputSection() {
 
     const isVowel = (letter) => {
         const vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+        if (letter === undefined) {
+            return false
+        }
         return vowels.includes(letter.toLowerCase())
     }
 
-    const handleClick = (word, letterIndex) => {
-        if (isVowel(word[letterIndex]) && word[letterIndex + 1] !== '\u0301') {
-            const accentedWord = word
-                .split('')
-                .map((letter, index) => {
-                    if (index === letterIndex) {
-                        return letter + '\u0301'
-                    }
-                    return letter
-                })
-                .join('')
-
-            const newText = text.replace(word, accentedWord)
-            setText(newText)
-            setAccentuatedText(newText)
-        } else if (word[letterIndex + 1] === '\u0301') {
-            const deaccentedWord =
-                word.slice(0, letterIndex) + word[letterIndex] + word.slice(letterIndex + 2)
-            const newText = text.replace(word, deaccentedWord)
-            setText(newText)
-            setAccentuatedText(newText)
-        }
+    const handleClick = (clickedWord, letterIndex) => {
+        const words = text.split(' ')
+        words.forEach((word, wordIndex) => {
+            if (word === clickedWord) {
+                if (isVowel(word[letterIndex]) && word[letterIndex + 1] !== '\u0301') {
+                    const accentedWord = word
+                        .split('')
+                        .map((letter, index) => {
+                            if (index === letterIndex) {
+                                return letter + '\u0301'
+                            }
+                            return letter
+                        })
+                        .join('')
+                    words[wordIndex] = accentedWord
+                } else if (word[letterIndex + 1] === '\u0301') {
+                    const deaccentedWord =
+                        word.slice(0, letterIndex) + word[letterIndex] + word.slice(letterIndex + 2)
+                    words[wordIndex] = deaccentedWord
+                }
+            }
+        })
+        const newText = words.join(' ')
+        setText(newText)
+        setAccentuatedText(newText)
     }
 
     const renderStanza = (stanza) => {
@@ -63,7 +69,7 @@ function InputSection() {
         <section className="content wrapper">
             <textarea className="content__textarea" onChange={(e) => setText(e.target.value)}></textarea>
             <button className="content__button" onClick={() => showText()}>
-                accentuate
+                Аccentuate
             </button>
             {accentuatedText && (
                 <>
